5f82e37e3e031dcd13f2d452d0b7381e67c517ba
abdennour <abdennour.oussaid9@gmail.com>
2022-08-16 20:45:18 +0100

    i have no idea what i did

--- dom.js
+++ dom.js
@@ -3,18 +3,25 @@ function createGrid() {
     let container=document.createElement("div");
     container.setAttribute("id","container");
     let input=document.getElementById("nbr-square").value;
-
+    
+if (input>=100 || input<=0) {
+    alert(`you enterd ${input},
+           you have to chose a number between 0 and 100`);
+}else{
     for (let i = input; i >0; i--) {
+        let div=document.createElement("div");
         let row=document.createElement("div");
             row.setAttribute("class","row");
             row.textContent=`i=${i}`;
+            div.appendChild(row);
         for(let j=input-1;j>0;j--){
         let colm=document.createElement("div");
             colm.setAttribute("class","colm");
             colm.textContent=`j=${j}`;
-        row.appendChild(colm);
+        div.appendChild(colm);
         }
-        container.appendChild(row);
+        container.appendChild(div);
     }
     body[0].appendChild(container);
+}
 }
