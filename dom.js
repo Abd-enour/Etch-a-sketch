54d126aac42de7858240c421c54b54919074aaa5
abdennour <abdennour.oussaid9@gmail.com>
2022-08-17 14:25:58 +0100

    i made the color input round

--- dom.js
+++ dom.js
@@ -6,20 +6,18 @@ function createGrid() {
     container.setAttribute("id","container");
     let input=document.getElementById("nbr-square").value;
     
-if (input>=100 || input<=0) {
+if (input>50 || input<=0) {
     alert(`you enterd ${input},
-           you have to chose a number between 0 and 100`);
+           you have to chose a number between 1 and 50`);
 }else{
     for (let i = input; i >0; i--) {
         let div=document.createElement("div");
         let row=document.createElement("div");
-            row.setAttribute("class","row");
-            row.textContent=`i=${i}`;
+            row.setAttribute("class","row row"+i);
             div.appendChild(row);
         for(let j=input-1;j>0;j--){
         let colm=document.createElement("div");
-            colm.setAttribute("class","colm");
-            colm.textContent=`j=${j}`;
+            colm.setAttribute("class","colm colm"+j);
         div.appendChild(colm);
         }
         container.appendChild(div);
