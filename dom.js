a58b2e5257488575f23c2a447d60ee6a1e152e19
abdennour <abdennour.oussaid9@gmail.com>
2022-08-18 15:31:47 +0100

    start with the logic of the game

--- dom.js
+++ dom.js
@@ -1,27 +1,27 @@
-function createGrid() {
-    let body =document.getElementsByTagName("body");
-    let secondTag= document.querySelectorAll("body > *");
-    if(secondTag[1] !=undefined){secondTag[1].remove();}
-    let container=document.createElement("div");
-    container.setAttribute("id","container");
-    let input=document.getElementById("nbr-square").value;
+let color =document.getElementsByClassName("inp")[0];
+console.log(color.value);
+
+let span=document.getElementById("span-range").textContent;
+console.log(span);
+
+let range=document.getElementsByTagName("input")[1];
+console.log(range.value);
+
+function colorMode() {
     
-if (input>50 || input<=0) {
-    alert(`you enterd ${input},
-           you have to chose a number between 1 and 50`);
-}else{
-    for (let i = input; i >0; i--) {
-        let div=document.createElement("div");
-        let row=document.createElement("div");
-            row.setAttribute("class","row row"+i);
-            div.appendChild(row);
-        for(let j=input-1;j>0;j--){
-        let colm=document.createElement("div");
-            colm.setAttribute("class","colm colm"+j);
-        div.appendChild(colm);
-        }
-        container.appendChild(div);
-    }
-    body[0].appendChild(container);
 }
+
+
+function rainbowMode() {
+    
+}
+
+
+
+function eraser() {
+    
+}
+
+function clear() {
+    
 }
