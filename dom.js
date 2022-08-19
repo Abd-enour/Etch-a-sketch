f18169c13570cc20b97fec2a721b5ee3e577b33a
abdennour <abdennour.oussaid9@gmail.com>
2022-08-19 14:41:36 +0100

    i fixed the logic behind the range button and the colorMode button

--- dom.js
+++ dom.js
@@ -1,25 +1,58 @@
-let color =document.getElementsByClassName("inp")[0];
-console.log(color.value);
-
-
+let rightContainer=document.querySelector(".grid");
 let range=document.getElementsByTagName("input")[1];
-range.addEventListener("click", ()=>{
+
+// the createDiv(nbr) function creates X*X grid dependding on the user choice.
+createDiv(16);
+function createDiv(nbr){
+    for (let i = Math.pow(nbr,2); i>0; i--) {
+        let div=document.createElement("div");
+        div.setAttribute("class","grid-cells");
+        rightContainer.appendChild(div);
+    }
+}
+// end of createDiv() function.
+
+// the setToDefault() function sets the grid to a 16x16 grid
+function setToDefault() {
+    rightContainer.innerHTML="";
     let span=document.getElementById("span-range").textContent=`${range.value} x ${range.value}`;
-    let rightContainer=document.getElementById("right-container");
     rightContainer.style.gridTemplateColumns=`repeat(${range.value},1fr)`;
     rightContainer.style.gridTemplateRows=`repeat(${range.value},1fr)`;
-    for (let i = range.value; i>0; i-) {
-        let div
-        
-    }
-});
+}
+// end of the setToDefualt() function
 
-let rightContainer=document.getElementById("right-container");
+range.addEventListener("click", ()=>{
+    setToDefault();    
+    createDiv(range.value);
+    let color =document.getElementsByClassName("inp")[0];  
+    /*  i think the code below is repeating .
+        so i need to put it inside a function.
+        i will do this next time inchllh. 
+    */
+    let gridCells=document.getElementsByClassName("grid-cells");
+    Array.from(gridCells).forEach((item)=>{
+        item.addEventListener("click", ()=>{
+            item.style=`background-color:${color.value}`;
+       });
+    });  
+    // end of the repeating code. :)
+});
 
 let colorMode=document.getElementsByClassName("inp")[1];
 colorMode.addEventListener("click",()=>{
     colorMode.setAttribute("class","fo inp");
+    let color =document.getElementsByClassName("inp")[0];
+    let gridCells=document.getElementsByClassName("grid-cells");
+    Array.from(gridCells).forEach((item)=>{
+        item.addEventListener("click", ()=>{
+            item.style=`background-color:${color.value}`;
+       });
+    });
+
 });
+
+
+
 
 let rainbowMode=document.getElementsByClassName("inp")[2];
 rainbowMode.addEventListener("click",()=>{
