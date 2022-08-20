3300506ac43b3b6be31d41f03fe05d1197d4b766
abdennour <abdennour.oussaid9@gmail.com>
2022-08-20 15:20:40 +0100

    stil freaking working on it for the 4th day

--- dom.js
+++ dom.js
@@ -1,9 +1,11 @@
 let rightContainer=document.querySelector(".grid");
 let range=document.getElementsByTagName("input")[1];
 let color =document.getElementById("inp-color");
+let gridCells=document.getElementsByClassName("grid-cells");
 
-// the createDiv(nbr) function creates X*X grid dependding on the user choice.
+gridEventColor(color)
 createDiv(16);
+// the createDiv(nbr) function creates nbr*nbr grid dependding on the user choice.
 function createDiv(nbr){
     for (let i = Math.pow(nbr,2); i>0; i--) {
         let div=document.createElement("div");
@@ -21,45 +23,50 @@ function setToDefault() {
     rightContainer.style.gridTemplateRows=`repeat(${range.value},1fr)`;
 }
 // end of the setToDefualt() function
-
 range.addEventListener("click", ()=>{
     setToDefault();    
     createDiv(range.value);
-    /*  i think the code below is repeating .
-        so i need to put it inside a function.
-        i will do this next time inchllh. 
-    */
-    let gridCells=document.getElementsByClassName("grid-cells");
-    Array.from(gridCells).forEach((item)=>{
-        item.addEventListener("click", ()=>{
-            item.style=`background-color:${color.value}`;
-       });
-    });  
-    // end of the repeating code. :)
+    gridEventColor(color)
 });
 
 let colorMode=document.getElementsByTagName("button")[0];
 colorMode.addEventListener("click",()=>{
-    let gridCells=document.getElementsByClassName("grid-cells");
-    Array.from(gridCells).forEach((item)=>{
-        item.addEventListener("click", ()=>{
-            item.style=`background-color:${color.value}`;
-       });
-    });
-
+    colorMode.classList.add("active");
+    rainbowMode.classList.remove("active");
+    eraser.classList.remove("active");  
+    gridEventColor(color);
 });
 
 let rainbowMode=document.getElementsByTagName("button")[1];
 rainbowMode.addEventListener("click",()=>{
-    
+    colorMode.classList.remove("active");
+    eraser.classList.remove("active");
+    rainbowMode.classList.add("active");
 });
 
 let eraser=document.getElementsByTagName("button")[2];
 eraser.addEventListener("click",()=>{
-    eraser.classList.toggle("active");
+    eraser.classList.add("active");
+    rainbowMode.classList.remove("active");
+    colorMode.classList.remove("active");
+  });
+  eraser.onclick=()=>{
+    Array.from(gridCells).forEach((item)=>{
+        item.addEventListener("click", ()=>{
+            item.style=`background-color:white`;
+       });
 });
+}
 
 let clear=document.getElementsByTagName("button")[3];
 clear.addEventListener("click",()=>{
     setToDefault();
+    createDiv(16);
 });
+
+function gridEventColor(backgroundColor){
+Array.from(gridCells).forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        item.style=`background-color:${backgroundColor.value}`;
+   });
+});}
